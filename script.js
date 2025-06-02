ab8237695b7356fbf33d3d46d66fc07efecbc20b
Jonas Rocha <jonasrpnstudy@gmail.com>
2025-06-02 12:42:22 -0300

    Aprendendo sobre arrays

---
+++ script.js
@@ -0,0 +1,19 @@
+/*
+Array
+
+Array é uma coleção ordenada de valores.
+
+Podemos comparar um array a uma lista, onde cada item da
+lista possui uma posição específica, conhecida como índice.
+*/
+
+/*
+Exemplo
+
+Pense em uma caixa de correio com vários compartimentos numerados.
+Cada compartimento é como uma posição no array, e dentro de cada
+compartimento, você pode armazenar algo, como uma carta.
+
+Os compartimentos são numerados de forma ordenada, começando do 0
+e indo até o número total de compartimentos menos um.
+*/
