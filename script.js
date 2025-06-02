174ce72ce8dcef6eba009e91a7eaa552a429302b
Jonas Rocha <jonasrpnstudy@gmail.com>
2025-06-02 12:52:44 -0300

    Criando um array com um construtor

--- script.js
+++ script.js
@@ -17,3 +17,20 @@ compartimento, você pode armazenar algo, como uma carta.
 Os compartimentos são numerados de forma ordenada, começando do 0
 e indo até o número total de compartimentos menos um.
 */
+
+// Criando Array com construtor.
+
+const newArray = new Array();
+// Mostando que tudo no JS é um objeto, até mesmo um array
+//console.log(typeof newArray);
+console.log(newArray);
+console.log("Jonas".length); //O "length" não é um método, é apenas uma propriedade de string que pode ser utilizada.
+console.log(newArray.length); //Como os Arrays são considerados uma caideia de caracteres, a propriedade length pode ser utilizada nele.
+
+// [] - Array
+// {} - Object
+
+// Cria o array com 10 posições vazias.
+const avaliable = new Array(10);
+console.log(avaliable);
+console.log(avaliable.length);
